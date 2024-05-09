6b6e563f7c7eb664ed9c2e98de9bc22d352fc8bb
Fredrik <fre-svee@hotmail.no>
2024-05-09 14:13:59 +0200

    Trying multivariate sine

--- AR_script.py
+++ AR_script.py
@@ -10,20 +10,39 @@ import sys
 import warnings 
 warnings.filterwarnings("ignore") 
 
-data = np.loadtxt(f'sine_data/sine_123_1000_1.csv', delimiter = ",",skiprows = 1)
-# data = data[0]
-data = pd.DataFrame(data)
-data = data[1]
-print(type(data))
+import os
+directory = os.path.join(os.curdir, "sine_data")
+file = 'sine_123_1000_9.csv'
 
-# Fit auto_arima function to dataset 
-stepwise_fit = auto_arima(data, start_p = 1, start_q = 1, 
+data = np.loadtxt(os.path.join(directory, file), delimiter = ",",skiprows = 1)
+dim = file.split('_')[3][0]
+print(f'dim is {dim}')
+
+data = pd.DataFrame(data)
+
+if dim == '1':
+    data = data[1]
+    best_model = auto_arima(data, start_p = 1, start_q = 1, 
                           max_p = 3, max_q = 3, 
                           start_P = 0, seasonal = True, 
                           d = None, D = 1, trace = True, 
                           error_action ='ignore',   # we don't want to know if an order does not work 
                           suppress_warnings = True,  # we don't want convergence warnings 
                           stepwise = True)           # set to stepwise 
+
+else:
+    # Fit auto_arima function to dataset
+    generated_sines = []
+    for feature in data.columns:
+        feat = data[feature]
+        best_model = auto_arima(feat, start_p = 1, start_q = 1, 
+                                max_p = 3, max_q = 3, 
+                                start_P = 0, seasonal = True, 
+                                d = None, D = 1, trace = True, 
+                                error_action ='ignore',   # we don't want to know if an order does not work 
+                                suppress_warnings = True,  # we don't want convergence warnings 
+                                stepwise = True)           # set to stepwise
+        forecast = pd.DataFrame(best_model.predict(n_periods = 5))
+        print(forecast)
+        
   
-# To print the summary 
-stepwise_fit.summary() 
