7391fb01dbd9509434b53f262c3eb4b8d17dd49d
Fredrik <fre-svee@hotmail.no>
2024-05-09 13:37:25 +0200

    dont specify montly seasonality

--- AR_script.py
+++ AR_script.py
@@ -16,9 +16,9 @@ data = pd.DataFrame(data)
 data = data[8]
 print(type(data))
 
-# Fit auto_arima function to AirPassengers dataset 
+# Fit auto_arima function to dataset 
 stepwise_fit = auto_arima(data, start_p = 1, start_q = 1, 
-                          max_p = 3, max_q = 3, m = 12, 
+                          max_p = 3, max_q = 3, 
                           start_P = 0, seasonal = True, 
                           d = None, D = 1, trace = True, 
                           error_action ='ignore',   # we don't want to know if an order does not work 
