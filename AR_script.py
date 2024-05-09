bd25914081080839370f2ba89303f2eaa40f8eec
Fredrik <fre-svee@hotmail.no>
2024-05-09 15:05:42 +0200

    generate all arima sines

--- AR_script.py
+++ AR_script.py
@@ -15,44 +15,54 @@ warnings.filterwarnings("ignore")
 import os
 directory = os.path.join(os.curdir, "sine_data")
 seq_len = 200
-file = 'sine_123_1000_9.csv'
+dims = [1, 2, 4, 6, 9]
 
-data = np.loadtxt(os.path.join(directory, file), delimiter = ",",skiprows = 1)
-dim = file.split('_')[3][0]
-print(f'dim is {dim}')
+for dim in dims:
+    file = f'sine_123_1000_{dim}.csv'
+    data = np.loadtxt(os.path.join(directory, file), delimiter = ",",skiprows = 1)
+    # dim = file.split('_')[3][0]
+    print(f'dim is {dim}')
 
-data = pd.DataFrame(data)
+    data = pd.DataFrame(data)
 
-if dim == '1':
-    data = data[1]
-    best_model = auto_arima(data, start_p = 1, start_q = 1, 
-                          max_p = 3, max_q = 3, 
-                          start_P = 0, seasonal = True, 
-                          d = None, D = 1, trace = True, 
-                          error_action ='ignore',   # we don't want to know if an order does not work 
-                          suppress_warnings = True,  # we don't want convergence warnings 
-                          stepwise = True)           # set to stepwise 
-
-else:
-    # Fit auto_arima function to dataset
-    generated_sines = []
-    for feature in data.columns:
-        feat = data[feature]
-        best_model = auto_arima(feat, start_p = 1, start_q = 1, 
-                                max_p = 3, max_q = 3, 
-                                start_P = 0, seasonal = True, 
-                                d = None, D = 1, trace = True, 
-                                error_action ='ignore',   # we don't want to know if an order does not work 
-                                suppress_warnings = True,  # we don't want convergence warnings 
-                                stepwise = True)           # set to stepwise
+    if dim == '1':
+        data = data[1]
+        best_model = auto_arima(data, start_p = 1, start_q = 1, 
+                            max_p = 3, max_q = 3, 
+                            start_P = 0, seasonal = True, 
+                            d = None, D = 1, trace = True, 
+                            error_action ='ignore',   # we don't want to know if an order does not work 
+                            suppress_warnings = True,  # we don't want convergence warnings 
+                            stepwise = True)           # set to stepwise
         forecast = pd.DataFrame(best_model.predict(n_periods = seq_len))
-        generated_sines.append(forecast.values.tolist())
-        # Save generated data to csv
-    x = datetime.datetime.now()
+        gen_list = forecast.values.tolist()
+        x = datetime.datetime.now()
 
-    timestamp = x.strftime("%d%m%y_%Hh%M")
-    filepath = f'synthetic_sines/arima_sine_{dim}_{seq_len}_{timestamp}.json'
-    with open(filepath, 'w') as file:
-        json.dump(generated_sines, file)
+        timestamp = x.strftime("%d%m%y_%Hh%M")
+        filepath = f'synthetic_sines/arima_sine_{dim}_{seq_len}_{timestamp}.json'
+        with open(filepath, 'w') as file:
+            json.dump(gen_list, file)
+
+    else:
+        # Fit auto_arima function to dataset
+        generated_sines = []
+        for feature in data.columns:
+            feat = data[feature]
+            best_model = auto_arima(feat, start_p = 1, start_q = 1, 
+                                    max_p = 3, max_q = 3, 
+                                    start_P = 0, seasonal = True, 
+                                    d = None, D = 1, trace = True, 
+                                    error_action ='ignore',   # we don't want to know if an order does not work 
+                                    suppress_warnings = True,  # we don't want convergence warnings 
+                                    stepwise = True)           # set to stepwise
+            forecast = pd.DataFrame(best_model.predict(n_periods = seq_len))
+            generated_sines.append(forecast.values.tolist())
+            # Save generated data to csv
+        x = datetime.datetime.now()
+
+        timestamp = x.strftime("%d%m%y_%Hh%M")
+        filepath = f'synthetic_sines/arima_sine_{dim}_{seq_len}_{timestamp}.json'
+        with open(filepath, 'w') as file:
+            json.dump(generated_sines, file)
         
   
